fa8e731bf7abece3817ab537d38bfc8bd6a21b53
makenneth <kenneth.km.ma@gmail.com>
2017-05-17 15:22:59 -0700

    Fixed association tests

--- package/lib/repository/association.py
+++ package/lib/repository/association.py
@@ -9,30 +9,33 @@ class Association:
     return cls._instance
 
   def __init__(self):
-    self.__assocations = {}
+    self.__associations = {}
 
   def __getattr__(self, name):
     return "Unrecognized association: {}".format(name)
 
-  def get_associations(self):
-    return self.__assocations
+  def get_associations(self, callee_class):
+    print(self.__associations)
+    if callee_class in self.__associations:
+      return self.__associations[callee_class]
+
+    return {}
 
   def class_to_table(self, class_name):
     return inflection.pluralize(inflection.underscore(class_name))
 
   def has_many(self, callee, assoc_table, **kwargs):
-    callee_class = callee.__class__.__name__
-    if callee_class in self.__assocations:
+    if callee in self.__associations:
       return
 
-    self.__assocations[callee_class] = {}
+    self.__associations[callee] = {}
     table_name = inflection.underscore(assoc_table)
-    primary_class = callee_class
+    primary_class = callee
     foreign_class = inflection.singularize(assoc_table) if "foreign_class" not in kwargs else kwargs["foreign_class"]
     p_key = "id" if "primary_key" not in kwargs else kwargs["primary_key"]
-    f_key = inflection.underscore(callee_class) + "_id" if "foreign_key" not in kwargs else kwargs["foreign_key"]
+    f_key = inflection.underscore(callee) + "_id" if "foreign_key" not in kwargs else kwargs["foreign_key"]
 
-    self.__assocations[callee_class][table_name] = {
+    self.__associations[callee][table_name] = {
       "type": "has_many",
       "primary_class": primary_class,
       "foreign_class": foreign_class,
@@ -41,18 +44,17 @@ class Association:
     }
 
   def belongs_to(self, callee, assoc_table, **kwargs):
-    callee_class = callee.__class__.__name__
-    if callee_class in self.__assocations:
+    if callee in self.__associations:
       return
 
-    self.__assocations[callee_class] = {}
+    self.__associations[callee] = {}
     table_name = inflection.underscore(inflection.pluralize(assoc_table))
     primary_class = assoc_table
-    foreign_class = callee_class if "foreign_class" not in kwargs else kwargs["foreign_class"]
+    foreign_class = callee if "foreign_class" not in kwargs else kwargs["foreign_class"]
     p_key = "id" if "primary_key" not in kwargs else kwargs["primary_key"]
     f_key = "{}_id".format(inflection.underscore(assoc_table)) if "foreign_key" not in kwargs else kwargs["foreign_key"]
 
-    self.__assocations[table_name] = {
+    self.__associations[callee][table_name] = {
       "type": "belongs_to",
       "primary_class": primary_class,
       "foreign_class": foreign_class,
--- package/tests/association_test.py
+++ package/tests/association_test.py
@@ -1,32 +1,36 @@
 import unittest
-from . import Association
+from . import AssociationRepo as Association
 
 class TestAssociationMethods(unittest.TestCase):
   def test_adding_default_has_many_association(self):
-    class GroupUser(Association): pass
-    associations = GroupUser()
-    associations.has_many("UserPosts")
+    associations = Association()
+    associations.has_many("GroupUser", "UserPosts")
 
-    self.assertTrue("user_posts" in associations._associations)
-    added_associations = associations._associations["user_posts"]
+    added_associations = associations.get_associations("GroupUser")
+    self.assertFalse(added_associations == {})
+    self.assertTrue("user_posts" in added_associations)
 
-    self.assertTrue(added_associations["foreign_key"] == "group_user_id")
-    self.assertTrue(added_associations["primary_key"] == "id")
-    self.assertTrue(added_associations["foreign_class"] == "UserPost")
-    self.assertTrue(added_associations["primary_class"] == "GroupUser")
+    user_posts_association = added_associations["user_posts"]
+    self.assertTrue(user_posts_association["type"] == "has_many")
+    self.assertTrue(user_posts_association["foreign_key"] == "group_user_id")
+    self.assertTrue(user_posts_association["primary_key"] == "id")
+    self.assertTrue(user_posts_association["foreign_class"] == "UserPost")
+    self.assertTrue(user_posts_association["primary_class"] == "GroupUser")
 
   def test_adding_default_belongs_to(self):
-    class UserPost(Association): pass
-    associations = UserPost()
-    associations.belongs_to("GroupUser")
+    associations = Association()
+    associations.belongs_to("UserPost", "GroupUser")
 
-    self.assertTrue("group_users" in associations._associations)
-    added_associations = associations._associations["group_users"]
+    added_associations = associations.get_associations("UserPost")
+    self.assertFalse(added_associations == {})
+    self.assertTrue("group_users" in added_associations)
 
-    self.assertTrue(added_associations["foreign_key"] == "group_user_id")
-    self.assertTrue(added_associations["primary_key"] == "id")
-    self.assertTrue(added_associations["primary_class"] == "GroupUser")
-    self.assertTrue(added_associations["foreign_class"] == "UserPost")
+    group_user_association = added_associations["group_users"]
+    self.assertTrue(group_user_association["type"] == "belongs_to")
+    self.assertTrue(group_user_association["foreign_key"] == "group_user_id")
+    self.assertTrue(group_user_association["primary_key"] == "id")
+    self.assertTrue(group_user_association["primary_class"] == "GroupUser")
+    self.assertTrue(group_user_association["foreign_class"] == "UserPost")
 
 if __name__ == "__main__":
   unittest.main()
