e06ffd7f9f104da79ad226fefef2f722152a4d50
makenneth <kenneth.km.ma@gmail.com>
2017-05-20 00:27:51 -0700

    Added helpers module

---
+++ package/lib/helpers/index.py
@@ -0,0 +1,7 @@
+import inflection
+
+def format_clause_value(val):
+  return str(val) if type(val) != str else "'{}'".format(val)
+
+def class_to_table(class_name):
+  return inflection.pluralize(inflection.underscore(class_name))
