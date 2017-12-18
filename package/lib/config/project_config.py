6ec429d085c7ecedbecdc6343c88ad9efe18882f
makenneth <kenneth.km.ma@gmail.com>
2017-12-18 12:13:59 -0800

    Added config folder; added loading of models at initialization

---
+++ package/lib/config/project_config.py
@@ -0,0 +1,31 @@
+import os, sys, importlib, re, inflection
+
+class Config:
+  _loaded = False
+  project_path = None
+
+  @classmethod
+  def set_project_path(cls, dirname, file):
+    file_dir = os.path.dirname(os.path.abspath(file))
+    cls.project_path = os.path.join(dirname, file_dir)
+    cls.load_files()
+
+  @classmethod
+  def load_files(cls):
+    if not cls._loaded:
+      name_re = re.compile("^(?!__)(.*).py$")
+      model_path = os.path.join(cls.project_path, "models")
+      sys.path.append(cls.project_path) # set path to be the main directory for loading modules
+
+      for name in os.listdir(model_path):
+        if name_re.match(name):
+          model = name[:-3]
+          model_class = inflection.camelize(model)
+          module = importlib.import_module("models.{}".format(model))
+
+          imported_class = getattr(module, model_class)
+          if not imported_class:
+            logger.error("Unable to find {} in {}, you may not be able to use associations before to initializing.".format(model_class, model))
+          else:
+            imported_class()
+
